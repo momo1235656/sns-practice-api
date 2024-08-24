73e7f0285bc360e9ee7a4b81d57a2f22be5109a8
momo <wisteria1235656@yahoo.co.jp>
2024-08-25 01:29:00 +0900

    first commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const authRoute = require("./routers/auth");
+const postRoute = require("./routers/posts");
+const usersRoute = require("./routers/users");
+const cors = require("cors");
+
+require("dotenv").config();
+
+const PORT = 8000;
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello world</h1>");
+});
+app.use(cors());
+app.use(express.json());
+app.use("/api/auth", authRoute);
+app.use("/api/posts", postRoute);
+app.use("/api/users", usersRoute);
+
+app.listen(PORT, () => console.log(`server is running on Port ${PORT}`));

541e8bacd65aed933e4f5b41541f774a889cb447
momo <wisteria1235656@yahoo.co.jp>
2024-08-25 01:48:34 +0900

    PORT

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 
 require("dotenv").config();
 
-const PORT = 8000;
+const PORT = process.env.PORT || 10000;
 
 app.get("/", (req, res) => {
   res.send("<h1>Hello world</h1>");
